80a031b704336cf2027d0bd1480c09b3331fe133
Pavel Borunov <8665691+mrneatly@users.noreply.github.com>
2025-03-11 21:43:50 +0200

    Implement container factory

---
+++ src/ContainerFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bicykl;
+
+use Bicykl\Strategies\AppStrategy;
+use League\Container\Container;
+
+class ContainerFactory
+{
+    public static function build(AppStrategy $strategy): Container
+    {
+        $bootstrap = $strategy
+            ->initContainer() // setting up a container
+            ->boot(); // booting the app (registering all required service providers)
+
+        return $bootstrap->getContainer(); // returning updated dependency container
+    }
+}
