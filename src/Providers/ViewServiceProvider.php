67fa719ca63b79f24ff66c303bce2547247f1c06
Pavel Borunov <8665691+mrneatly@users.noreply.github.com>
2025-03-12 22:13:32 +0200

    Implement togglable Twig debug mode

--- src/Providers/ViewServiceProvider.php
+++ src/Providers/ViewServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace Bicykl\Providers;
 
+use Bicykl\Config;
 use Bicykl\View;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 class ViewServiceProvider extends AbstractServiceProvider
@@ -20,10 +22,16 @@ class ViewServiceProvider extends AbstractServiceProvider
         $this->getContainer()->add(View::class, function () {
             $loader = new FilesystemLoader($this->viewsFolderPath);
 
+            $debugMode = $this->getContainer()->get(Config::class)->get('app.debug');
+
             $twig = new Environment($loader, [
                 'cache' => false,
-                'debug' => true,
+                'debug' => $debugMode,
             ]);
+
+            if ($debugMode) {
+                $twig->addExtension(new DebugExtension());
+            }
 
             return new View($twig);
         })
