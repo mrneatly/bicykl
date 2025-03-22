b5d72aed416a0ed4fad8127d0f43684fded855b3
mrneatly <8665691+mrneatly@users.noreply.github.com>
2025-03-22 20:51:00 +0200

    Move Twig prep logic to a boot() method

--- src/Providers/ViewServiceProvider.php
+++ src/Providers/ViewServiceProvider.php
@@ -5,35 +5,41 @@ namespace Bicykl\Providers;
 use Bicykl\Config;
 use Bicykl\View;
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
-class ViewServiceProvider extends AbstractServiceProvider
+class ViewServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
 {
+    protected Environment $twig;
+
     public function __construct(
         protected string $viewsFolderPath,
     )
     {
     }
 
+    public function boot(): void
+    {
+        $loader = new FilesystemLoader($this->viewsFolderPath);
+
+        $debugMode = $this->getContainer()->get(Config::class)->get('app.debug');
+
+        $this->twig = new Environment($loader, [
+            'cache' => false,
+            'debug' => $debugMode,
+        ]);
+
+        if ($debugMode) {
+            $this->twig->addExtension(new DebugExtension());
+        }
+    }
+
     public function register(): void
     {
         $this->getContainer()->add(View::class, function () {
-            $loader = new FilesystemLoader($this->viewsFolderPath);
-
-            $debugMode = $this->getContainer()->get(Config::class)->get('app.debug');
-
-            $twig = new Environment($loader, [
-                'cache' => false,
-                'debug' => $debugMode,
-            ]);
-
-            if ($debugMode) {
-                $twig->addExtension(new DebugExtension());
-            }
-
-            return new View($twig);
+            return new View($this->twig);
         })
             ->setShared();
     }
